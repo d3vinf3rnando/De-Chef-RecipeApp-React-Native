dc1fec34d6cd8897c8f8023598b9a98edc07083d
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2023-02-28 12:36:52 +0530

    code update image

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView,Image } from 'react-native';
 
 export default function App() {
+console.log('App Executed!!!')
+
+const handlePress =()=> console.log("liNK PRESSED") //onpress when big functions
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+
+      <Text onPress={handlePress}>hello world!</Text>
+      <Text onPress={()=>console.log('Congrats')}>This is my first react native app</Text>
+
+      <Image 
+      style={styles.myimage}
+      source={require('./assets/my.png')}/>
+
       <StatusBar style="auto" />
-    </View>
+
+      {/* <View>
+        <Text>Hello this is new view</Text>
+      </View> */}
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    justifyContent:'center',
     alignItems: 'center',
-    justifyContent: 'center',
+
+    
   },
+
+  myimage:{
+    width:'20%',
+    height:'10%',
+  }
 });
