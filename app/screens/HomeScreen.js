e359960947778c10628a38fd730eda86cc29030f
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2023-03-08 23:56:24 +0530

    cover added

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageBackground ,StyleSheet,View,Text,Button,TouchableOpacity,SafeAreaView, Image} from 'react-native';
+import { ImageBackground ,StyleSheet,View,Text,Button,TouchableOpacity,SafeAreaView, Image, TextInput,SearchBar} from 'react-native';
 
 function HomeScreen({navigation, route}) {
     return (
@@ -7,8 +7,20 @@ function HomeScreen({navigation, route}) {
             
                 <Text style={styles.head}>Discover Best{"\n"}Recipes</Text>
                 <Image style={styles.profile} source={require('../assets/PROFILE.png')} />
-                
 
+                <View style={styles.searchview}>
+                    <TextInput style={styles.search} placeholder='Search here...'></TextInput>
+                </View>
+
+                <Text>Just For You</Text>
+
+                <View style={styles.coverview}>
+                    <Image source={require('../assets/cover.png')}/>
+                    <Text>15 best pasta recipes from chef John</Text>
+                </View>
+
+
+                
 
             
         </SafeAreaView>
@@ -25,8 +37,26 @@ const styles = StyleSheet.create({
     },
 
     profile: {
-        top:-90,
+        top:'-20%',
         left:'80%'
+    },
+
+    search:{
+        
+        backgroundColor:'#EFEFEF',
+        height:60,
+        paddingLeft:20,
+        borderRadius:15,
+        
+    },
+
+    searchview:{
+        paddingHorizontal:20,
+        bottom:'10%'
+    },
+
+    coverview:{
+        bottom:20
     }
 })
 
